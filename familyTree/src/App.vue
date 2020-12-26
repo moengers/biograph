fd4bc837281b42e0ce2578ff682e6ab2de0cd608
momo <hohenheim@mailbox.org>
2020-12-27 00:35:21 +0100

    Somehow the examle works

--- familyTree/src/App.vue
+++ familyTree/src/App.vue
@@ -1,27 +1,43 @@
 <template>
   <div id="app">
-    <h1>Familienstammbaum</h1>
-    <fam-tree />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <PackChart :tweetData=loadData />
   </div>
 </template>
 
 <script>
-import FamTree from './familytree.vue';
+
+// https://levelup.gitconnected.com/d3-js-and-vue-js-7a6a721eb79f
+import * as d3 from 'd3';
+
+import PackChart from './components/familytree.vue';
 
 export default {
-  data() {
-    return {};
-  },
-  name: 'App',
+  name: 'app',
   components: {
-    FamTree,
+    PackChart,
+  },
+  data() {
+    return {
+      loadData: [],
+    };
+  },
+  mounted() {
+    console.log('App loaded');
+    this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      const data = await d3.json('./tweets.json');
+      this.loadData = data;
+    },
   },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- familyTree/src/components/familytree.vue
+++ familyTree/src/components/familytree.vue
@@ -1,8 +1,24 @@
 <template>
-  <div id="familyTree">
-        <div id="graph"></div>
-        <h1>xxx</h1>
-        <!-- <div>{{treeJason}}</div> -->
+  <div>
+    <h1>Circle Pack in D3</h1>
+    <h2>{{ msg }}</h2>
+    <svg
+      :height='height'
+      :width='width'
+    >
+      <g transform="translate(50,50)">
+        <circle
+          v-for="c in output"
+          :key="c.id"
+          :r="c.r"
+          :cx="c.x"
+          :cy="c.y"
+          :fill="c.fill"
+          :stroke="c.stroke"
+        >
+        </circle>
+      </g>
+    </svg>
   </div>
 </template>
 
@@ -51,10 +67,55 @@ console.log(treeJson);
 
 export default {
   data() {
-    return { treeJson };
+    return {
+      msg: 'Hi from Chart Component',
+      height: 600,
+      width: 600,
+    };
   },
   name: 'FamilyTree',
   components: {},
+  props: ['tweetData'],
+  created() {
+    this.colourScale = d3
+      .scaleOrdinal()
+      .range(['#5EAFC6', '#FE9922', '#93c464', '#75739F']);
+  },
+  computed: {
+    packData() {
+      const nestedTweets = d3
+        .nest()
+        .key((d) => d.user)
+        .entries(this.tweetData);
+
+      const packableTweets = { id: 'All Tweets', values: nestedTweets };
+      return d3
+        .hierarchy(packableTweets, (d) => d.values)
+        .sum((d) => (d.retweets ? d.retweets.length + d.favorites.length + 1 : 1));
+    },
+    output() {
+      return this.packChart();
+    },
+  },
+  methods: {
+    packChart() {
+      const packChart = d3.pack();
+      packChart.size([500, 500]);
+      packChart.padding(10);
+      const output = packChart(this.packData).descendants();
+      return output.map((d, i) => {
+        const fill = this.colourScale(d.depth);
+        return {
+          id: i + 1,
+          r: d.r,
+          x: d.x,
+          y: d.y,
+          fill,
+          stroke: 'grey',
+        };
+      });
+    },
+  },
 };
 </script>
 
